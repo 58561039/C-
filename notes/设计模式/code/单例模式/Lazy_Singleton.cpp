9eaccb1b83ffcbbb565220d8a9674399c74ff390
58561039 <71536618+58561039@users.noreply.github.com>
2025-08-27 20:49:48 +0800

    Update Lazy_Singleton.cpp

--- notes/设计模式/code/单例模式/Lazy_Singleton.cpp
+++ notes/设计模式/code/单例模式/Lazy_Singleton.cpp
@@ -2,7 +2,7 @@
 //write by lzc
 //以日志类为例，练习单例模式中的懒汉模式
 
-
+//第一种写法
 class Log{
     public:
         static Log& GetInstance(){
@@ -16,3 +16,27 @@ class Log{
         Log(const Log& log)=delete;
         Log& operator=(const Log& log)=delete;
 };
+
+//第二种写法
+static std::once_flag once;
+
+class LLog{
+    public:
+        void Print(std::string s){}
+        static LLog& GetInstance(){
+            std::call_once(once,init);
+            return *instance;
+        }
+        static void init(){
+            if(!instance){
+                instance=new LLog();
+            }
+        }
+    private:
+        LLog(){}
+        LLog(const LLog& llog)=delete;
+        LLog& operator=(const LLog& llog)=delete;
+        static LLog *instance;
+};
+
+LLog* LLog::instance=nullptr;
