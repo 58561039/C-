d931a50f9e27007be6ba9a2e397902fc76d270cf
58561039 <71536618+58561039@users.noreply.github.com>
2025-08-27 18:58:27 +0800

    Update Eager_Singleton.cpp

--- notes/设计模式/code/单例模式/Eager_Singleton.cpp
+++ notes/设计模式/code/单例模式/Eager_Singleton.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<string>
+//write by lzc
+//以日志类为例，练习单例模式中的饿汉模式
 
+class Log{
+    private:
+        Log(){}//构造函数
+        Log(const Log& log)=delete;//复制构造函数被删除
+        Log& operator=(const Log& log)=delete;//用operator重载=也不行
+        
+        static Log instance;//静态实例，在事先就声明
+
+
+    public:
+        void Print(std::string s){}//打印日志
+        static Log& GetInstance(){//获取实例
+            return instance;
+        }
+};
