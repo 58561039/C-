df5f6f41b8a5bceb9ccfee70766c6b380888f0ff
58561039 <71536618+58561039@users.noreply.github.com>
2025-08-27 19:03:49 +0800

    Update Lazy_Singleton.cpp

--- notes/设计模式/code/单例模式/Lazy_Singleton.cpp
+++ notes/设计模式/code/单例模式/Lazy_Singleton.cpp
@@ -1,1 +1,18 @@
+#include<string>
+//write by lzc
+//以日志类为例，练习单例模式中的懒汉模式
 
+
+class Log{
+    public:
+        static Log& GetInstance(){
+            static Log instance;
+            return instance;
+        }
+        void Print(std::string s){}
+
+    private:
+        Log(){}
+        Log(const Log& log)=delete;
+        Log& operator=(const Log& log)=delete;
+};
